692acd5d9ba5a4fa18deacc308c4ab7e27df1cc7
Leezard <lfj200411@163.com>
2017-11-16 18:51:53 +0800

    ...

--- Assets/WaterVersionTest/GpuFft.cs
+++ Assets/WaterVersionTest/GpuFft.cs
@@ -20,30 +20,16 @@ namespace WaterVersionTest {
 
         public void ChangeWind(Vector2 wind) {
             H0.SetFloats("Wind", wind.x, wind.y);
-            H0.Dispatch(_h0Kernel, Size / 8, Size / 8, 1);
+            H0.Dispatch(_h0Kernel, Size / 16, Size / 16, 1);
         }
 
         //public RenderTexture hkt, dx, dz;
-        
-
-        public Texture2D h0Test;
         private void Start() {
             Prepare();
-            H0.Dispatch(_h0Kernel, Size / 8, Size / 8, 1);
+            H0.Dispatch(_h0Kernel, Size / 16, Size / 16, 1);
             GetComponent<Renderer>().sharedMaterial.SetTexture("_VerticesTex", HktDtTargetR);
             GetComponent<Renderer>().sharedMaterial.SetTexture("_NormalsTex", StTargetR);
             GetComponent<Renderer>().sharedMaterial.SetInt("_HeightMapSize", Size);
-            h0Test = new Texture2D(516, 513, TextureFormat.RGFloat, false);
-            Vector2[] out_h0 = new Vector2[516 * 513];
-//            for (int i = 0; i < 516*513; i++) {
-//                out_h0[i] = Vector2.zero;
-//            }
-            float[] omega = new float[516 * 513];
-            InitHeightMap(new OceanParameter(), ref out_h0, ref omega);
-            for (int i = 0; i < 516 * 513; i++) {
-                h0Test.SetPixel(i / 516, i % 516, new Color(out_h0[i].x, out_h0[i].y, 0, 0));
-            }
-            h0Test.Apply();
         }
 
         public RenderTexture HktDtTargetR, HktDtTargetI, StTargetR, StTargetI;
@@ -152,104 +138,6 @@ namespace WaterVersionTest {
             _fftRowKernel = FftRow.FindKernel("Butterfly");
 
             _fftColKernel = FftCol.FindKernel("Butterfly");
-        }
-
-
-        private float Gauss() {
-            float u1 = Random.value;
-            float u2 = Random.value;
-            if (u1 < 1e-6f)
-                u1 = 1e-6f;
-            return Mathf.Sqrt(-2 * Mathf.Log(u1)) * Mathf.Cos(2 * Mathf.PI * u2);
-        }
-
-        // Phillips Spectrum
-        // K: normalized wave vector, W: wind direction, v: wind velocity, a: amplitude constant
-        private float Phillips(Vector2 K, Vector2 W, float v, float a, float dir_depend) {
-            // largest possible wave from constant wind of velocity v
-            float l = v * v / (Physics.gravity.magnitude * 100);
-            // damp out waves with very small length w << l
-            float w = l / 1000;
-
-            float Ksqr = K.x * K.x + K.y * K.y;
-            float Kcos = K.x * W.x + K.y * W.y;
-            float phillips = a * Mathf.Exp(-1 / (l * l * Ksqr)) / (Ksqr * Ksqr * Ksqr) * (Kcos * Kcos);
-
-            // filter out waves moving opposite to wind
-            if (Kcos < 0)
-                phillips *= dir_depend;
-
-            // damp out waves with very small length w << l
-            return phillips * Mathf.Exp(-Ksqr * w * w);
-        }
-
-        public class OceanParameter {
-            // Must be power of 2.
-            public int dmap_dim = 512;
-
-            // Typical value is 1000 ~ 2000
-            public float patch_length = 2000;
-
-            // Adjust the time interval for simulation.
-            public float time_scale = 0.8f;
-
-            // Amplitude for transverse wave. Around 1.0
-            public float wave_amplitude = 0.35f;
-
-            // Wind direction. Normalization not required.
-            public Vector2 wind_dir = new Vector2(0.8f, 0.6f);
-
-            // Around 100 ~ 1000
-            public float wind_speed = 600f;
-
-            // This value damps out the waves against the wind direction.
-            // Smaller value means higher wind dependency.
-            public float wind_dependency = 0.07f;
-
-            // The amplitude for longitudinal wave. Must be positive.
-            public float choppy_scale = 1.3f;
-        };
-
-        private const float HALF_SQRT_2 = 0.7071068f;
-
-        private void InitHeightMap(OceanParameter oceanParameter, ref Vector2[] out_h0, ref float[] out_omega) {
-            int i, j;
-            Vector2 K, Kn;
-
-            Vector2 wind_dir = oceanParameter.wind_dir.normalized;
-            float a = oceanParameter.wave_amplitude * 1e-7f; // It is too small. We must scale it for editing.
-            float v = oceanParameter.wind_speed;
-            float dir_depend = oceanParameter.wind_dependency;
-
-            int height_map_dim = oceanParameter.dmap_dim;
-            float patch_length = oceanParameter.patch_length;
-
-            // initialize random generator.
-
-            for (i = 0; i <= height_map_dim; i++) {
-                // K is wave-vector, range [-|DX/W, |DX/W], [-|DY/H, |DY/H]
-                K.y = (-height_map_dim / 2.0f + i) * (2 * Mathf.PI / patch_length);
-
-                for (j = 0; j <= height_map_dim; j++) {
-                    K.x = (-height_map_dim / 2.0f + j) * (2 * Mathf.PI / patch_length);
-
-                    float phil = (K.x == 0 && K.y == 0) ? 0 : Mathf.Sqrt(Phillips(K, wind_dir, v, a, dir_depend));
-
-                    out_h0[i * (height_map_dim + 4) + j].x = phil * Gauss() * HALF_SQRT_2;
-                    out_h0[i * (height_map_dim + 4) + j].y = phil * Gauss() * HALF_SQRT_2;
-
-                    // The angular frequency is following the dispersion relation:
-                    //            out_omega^2 = g*k
-                    // The equation of Gerstner wave:
-                    //            x = x0 - K/k * A * sin(dot(K, x0) - sqrt(g * k) * t), x is a 2D vector.
-                    //            z = A * cos(dot(K, x0) - sqrt(g * k) * t)
-                    // Gerstner wave shows that a point on a simple sinusoid wave is doing a uniform circular
-                    // motion with the center (x0, y0, z0), radius A, and the circular plane is parallel to
-                    // vector K.
-                    out_omega[i * (height_map_dim + 4) + j] =
-                        Mathf.Sqrt((Physics.gravity.magnitude * 100) * Mathf.Sqrt(K.x * K.x + K.y * K.y));
-                }
-            }
         }
     }
 }
---
+++ Assets/WaterVersionTest/GpuFftTest.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+
+namespace WaterVersionTest {
+    public class GpuFftTest : MonoBehaviour {
+        public ComputeShader HktTest;
+        public Texture2D H0PlusOmega;
+        private void Start() {
+            H0PlusOmega = new Texture2D(512, 512, TextureFormat.RGFloat, false);
+            Color[] h0PlusOmega = new Color[512 * 512];
+            InitHeightMap(ref h0PlusOmega);
+            H0PlusOmega.SetPixels(h0PlusOmega);
+            H0PlusOmega.Apply();
+        }
+
+
+        // Must be power of 2.
+        public int Size = 512;
+
+        // Typical value is 1000 ~ 2000
+        public float Length = 2000;
+
+        // Adjust the time interval for simulation.
+        public float TimeScale = 0.8f;
+
+        // Amplitude for transverse wave. Around 1.0
+        public float WaveAmplitude = 0.35f;
+
+        // Wind direction. Normalization not required.
+        public Vector2 WindDir = new Vector2(0.8f, 0.6f);
+
+        // Around 100 ~ 1000
+        public float WindSpeed = 600f;
+
+        // This value damps out the waves against the wind direction.
+        // Smaller value means higher wind dependency.
+        public float WindDependency = 0.07f;
+
+        // The amplitude for longitudinal wave. Must be positive.
+        public float ChoppyScale = 1.3f;
+
+        private const float HalfSqrt2 = 0.7071068f;
+
+        private void InitHeightMap(ref Color[] h0PlusOmega) {
+            Vector2 windDir = WindDir.normalized;
+            for (int i = 0; i < Size; i++) {
+                // K is wave-vector, range [-|DX/W, |DX/W], [-|DY/H, |DY/H]
+                Vector2 k;
+                k.y = (-Size / 2.0f + i) * (2 * Mathf.PI / Length);
+
+                for (int j = 0; j < Size; j++) {
+                    k.x = (-Size / 2.0f + j) * (2 * Mathf.PI / Length);
+
+                    float phil = 0;
+                    if (Mathf.Abs(k.x) > 1e-6 || Mathf.Abs(k.y) > 1e-6) {
+                        phil = Mathf.Sqrt(Phillips(k, windDir, WindSpeed, WaveAmplitude, WindDependency));
+                    }
+
+                    //out_h0[i * (Size + 4) + j].x = phil * Gauss() * HALF_SQRT_2;
+                    //out_h0[i * (Size + 4) + j].y = phil * Gauss() * HALF_SQRT_2;
+                    h0PlusOmega[i * (Size + 0) + j].r = phil * Gauss() * HalfSqrt2;
+                    h0PlusOmega[i * (Size + 0) + j].g = phil * Gauss() * HalfSqrt2;
+
+                    // The angular frequency is following the dispersion relation:
+                    //            out_omega^2 = g*k
+                    // The equation of Gerstner wave:
+                    //            x = x0 - K/k * A * sin(dot(K, x0) - sqrt(g * k) * t), x is a 2D vector.
+                    //            z = A * cos(dot(K, x0) - sqrt(g * k) * t)
+                    // Gerstner wave shows that a point on a simple sinusoid wave is doing a uniform circular
+                    // motion with the center (x0, y0, z0), radius A, and the circular plane is parallel to
+                    // vector K.
+                    h0PlusOmega[i * (Size + 0) + j].b =
+                        Mathf.Sqrt((Physics.gravity.magnitude * 100) * Mathf.Sqrt(k.x * k.x + k.y * k.y));
+                }
+            }
+        }
+
+        private float Gauss() {
+            float u1 = Random.value;
+            float u2 = Random.value;
+            if (u1 < 1e-6f)
+                u1 = 1e-6f;
+            return Mathf.Sqrt(-2 * Mathf.Log(u1)) * Mathf.Cos(2 * Mathf.PI * u2);
+        }
+
+        // Phillips Spectrum
+        // K: normalized wave vector, W: wind direction, v: wind velocity, a: amplitude constant
+        private float Phillips(Vector2 k, Vector2 windDir, float v, float a, float dirDepend) {
+            // largest possible wave from constant wind of velocity v
+            float l = v * v / (Physics.gravity.magnitude * 100);
+            // damp out waves with very small length w << l
+            float w = l / 1000;
+
+            float ksqr = k.x * k.x + k.y * k.y;
+            float kcos = k.x * windDir.x + k.y * windDir.y;
+            float phillips = a * Mathf.Exp(-1 / (l * l * ksqr)) / (ksqr * ksqr * ksqr) * (kcos * kcos);
+
+            // filter out waves moving opposite to wind
+            if (kcos < 0)
+                phillips *= dirDepend;
+
+            // damp out waves with very small length w << l
+            return phillips * Mathf.Exp(-ksqr * w * w);
+        }
+    }
+}
